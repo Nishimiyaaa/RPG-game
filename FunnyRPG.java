9d150f61fdb59697c887c9a74542f8e5aee28fbe
Nishimiya <xiaoqi060@gmail.com>
2021-01-30 00:41:33 -0500

    Userinput

--- FunnyRPG.java
+++ FunnyRPG.java
@@ -4,14 +4,17 @@ public class FunnyRPG {
    public static void main(String []args) {  
    
       Scanner input = new Scanner(System.in); 
-   
+      
       while (true) {
          System.out.println("Would you like to Attack? Yes or No?");
          String user = input.next();
          
-         
+         if (user == "Yes") {
+         //attack statements go under 
+         }
+         else 
+            System.out.println("COMING OUT!");         
       }
-   
    }
 }
 
