24dc2060f40b5f637cb586eb0fbe26b3268f95b3
Nishimiya <xiaoqi060@gmail.com>
2021-01-30 01:05:13 -0500

    Checks if the user says yes or no

--- FunnyRPG.java
+++ FunnyRPG.java
@@ -4,16 +4,18 @@ public class FunnyRPG {
    public static void main(String []args) {  
    
       Scanner input = new Scanner(System.in); 
+      toLowerCase 
       
       while (true) {
          System.out.println("Would you like to Attack? Yes or No?");
          String user = input.next();
          
-         if (user == "Yes") {
-         //attack statements go under 
+         if (input.next().toLowerCase().equals("Yes")) { //checks for the user's input, changes all the letters to lower case, then finds if the statement user put in is equal to (" ")
+            //attack statements go under
          }
-         else 
-            System.out.println("COMING OUT!");         
+         else if (input.next().toLowerCase().equals("No")) {  
+            System.out.println("COMING OUT!");     
+         }    
       }
    }
 }
