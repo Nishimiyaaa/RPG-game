8f723984948c70629fb3fc385b81a3897d2e22d9
NookRitzia <56681647+NookRitzia@users.noreply.github.com>
2021-01-30 00:31:44 -0500

    Monster Changes (Get Methods)

--- Monster.java
+++ Monster.java
@@ -1,6 +1,6 @@
 public class Monster {
 
-   private string name;
+   private String name;
    private double experience;
    private double health;
    private double defense;
@@ -12,6 +12,34 @@ public class Monster {
    
    }
    
+   public String getName() // Get Monster Name
+   {
+      return name;
+   }
    
+   public double getExperience() // Get Experience Of Monster
+   {
+      return experience;
+   }
+   
+   public double getHealth() // Get Monster Health
+   {
+      return health;
+   }
+   
+   public double getDefense() // Get Monster Defense
+   {
+      return defense;
+   }
+   
+   public int getLevel() // Get Monster Level
+   {
+      return level;
+   }
+   
+   public int getDeaths() // Get Monster Deaths
+   {
+      return deaths;
+   }
    
 }
