a066e095a320d60c031df99d0a498614e69e8783
NookRitzia <56681647+NookRitzia@users.noreply.github.com>
2021-01-30 00:27:15 -0500

    Update Monster.java

--- Monster.java
+++ Monster.java
@@ -1,2 +1,17 @@
 public class Monster {
+
+   private string name;
+   private double experience;
+   private double health;
+   private double defense;
+   private int level;
+   private int deaths;
+   
+   public Monster()
+   {
+   
+   }
+   
+   
+   
 }
