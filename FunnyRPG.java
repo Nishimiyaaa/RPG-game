c2542e72efea9663f32fdac801bc4f135c8c3683
Nishimiya <xiaoqi060@gmail.com>
2021-01-30 00:34:46 -0500

    User Attack?

--- FunnyRPG.java
+++ FunnyRPG.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 public class FunnyRPG {
    public static void main(String []args) {  
+   
+      Scanner input = new Scanner(System.in); 
+   
+      while (true) {
+         System.out.println("Would you like to Attack? Yes or No?");
+         String user = input.next();
+         
+         
+      }
    
    }
 }
 
-if (Defeated%2 == 0) {
-prints everything 
-}
+//if (Defeated%2 == 0) {
+//prints everything 
+//}
