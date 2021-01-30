be5b88e9aaf5a26d3c512b43656b6a706e6892ad
Asterito <78177329+Asterito@users.noreply.github.com>
2021-01-30 01:12:27 -0500

    human?

--- Human.java
+++ Human.java
@@ -1,2 +1,30 @@
-public class Human {
+public class Human 
+{
+   public static void main (String[] args)
+   {
+      int plevel = 1;
+      int pAttack = 5, pDefense = 5;
+      double pHealth = 100;
+      double pExp = 0, rExp = 100;
+   
+      if (pExp >= 100)
+      {
+      plevel++;
+      pExp = pExp * 1.25;
+      pAttack = pAttack + 2;
+      pDefense = pDefense +2;
+      pHealth = pHealth * 1.5;
+      
+      
+      
+      
+      
+      
+      } 
+   
+   
+   
+   }
+
+
 }
