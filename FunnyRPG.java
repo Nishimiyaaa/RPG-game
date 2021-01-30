60f67fed01d421d9b4594026814a5cda1368dcf8
Nishimiya <xiaoqi060@gmail.com>
2021-01-30 01:05:34 -0500

    Changes Cap Y and N

--- FunnyRPG.java
+++ FunnyRPG.java
@@ -10,10 +10,10 @@ public class FunnyRPG {
          System.out.println("Would you like to Attack? Yes or No?");
          String user = input.next();
          
-         if (input.next().toLowerCase().equals("Yes")) { //checks for the user's input, changes all the letters to lower case, then finds if the statement user put in is equal to (" ")
+         if (input.next().toLowerCase().equals("yes")) { //checks for the user's input, changes all the letters to lower case, then finds if the statement user put in is equal to (" ")
             //attack statements go under
          }
-         else if (input.next().toLowerCase().equals("No")) {  
+         else if (input.next().toLowerCase().equals("no")) {  
             System.out.println("COMING OUT!");     
          }    
       }
